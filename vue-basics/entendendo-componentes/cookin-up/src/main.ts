e7d2b90d059a9bef5c277ab7035d4291aa86e7dd
Gabriel Erick <gabrielmmve13@gmail.com>
2026-05-26 17:53:23 -0300

    aprendendo sobre 'type=module' e import em ts

--- vue-basics/entendendo-componentes/cookin-up/src/main.ts
+++ vue-basics/entendendo-componentes/cookin-up/src/main.ts
@@ -1,3 +1,4 @@
+// importando os estilos globais da aplicaćão aqui, que estão no nosso arquivo .css, como estamos trazendo aqui no ts a estilizaćão da aplicaćão, onde chamarmos esse arquivo ts, ele trará também as estilizaćões presentes no arquivo main.css dentro da pasta assets (tanto que, se repararmos, no momento atual, nosso index.html esta estilizado quando rodamos npm run dev no terminal, e em nenhum lugar daquele código está presente a chamada de um arquivo de estilizaćão, pois esta chamada é feita aqui no ts)
 import './assets/main.css'
 
 import { createApp } from 'vue'
