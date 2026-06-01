5a52086ed80e3e42890158369988d15575b3bcd9
Gabriel Erick <gabrielmmve13@gmail.com>
2026-06-01 18:28:29 -0300

    entendendo imports e manipulando primeiro componente

--- vue-basics/entendendo-componentes/cookin-up/src/App.vue
+++ vue-basics/entendendo-componentes/cookin-up/src/App.vue
@@ -1,3 +1,22 @@
+<!-- até o momento atual, esse é o componente "raiz" da nossa aplicaćão -->
+<!-- componente == qualquer arquivo que termine com a extensão .vue (ex: Exercicio.vue), podendo conter: lógica (através do JS/TS), HTML ou estilos (css/scss) -->
 <template>
-  <h1>Meu primeiro projeto Vue!</h1>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="./assets/images/logo.svg" alt="Logo do Cookin' Up" class="logo">
+      <!-- dentro da "configs padrão" que o curso me entregou, esta classe já foi configurada previamente -->
+      <p class="cabecalho-lg frase-cabecalho">
+        <span class="texto-verde">
+          Um banquete de ideias para
+        </span>
+        despertar o chef que há em você!
+      </p>
+
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
+    </div>
+
+    <img src="./assets/images/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro." class="foto-banner">
+  </header>
 </template>
--- vue-basics/entendendo-componentes/cookin-up/src/main.ts
+++ vue-basics/entendendo-componentes/cookin-up/src/main.ts
@@ -1,7 +1,12 @@
 // importando os estilos globais da aplicaćão aqui, que estão no nosso arquivo .css, como estamos trazendo aqui no ts a estilizaćão da aplicaćão, onde chamarmos esse arquivo ts, ele trará também as estilizaćões presentes no arquivo main.css dentro da pasta assets (tanto que, se repararmos, no momento atual, nosso index.html esta estilizado quando rodamos npm run dev no terminal, e em nenhum lugar daquele código está presente a chamada de um arquivo de estilizaćão, pois esta chamada é feita aqui no ts)
 import './assets/main.css'
-
+// importando funćão (import) "createApp" do (from) pacote "vue", funćão importante, usada para montar o projeto pela 1 vez
+// como é uma function, está entre {}
 import { createApp } from 'vue'
+// importando (import) App do caminho (from) './App.vue'
+// App == componente
 import App from './App.vue'
-
+// chamando a function que importamos (createApp), passando como parâmetro noso outro import (App)
+// a partir de createApp chamamos outro método (mount) dando como nome: #app (que por debaixo dos panos, devido a # é considerado um seletor css (== id em termos de css, ao invés de ser uma classe, é um id) e iremos manipular em nosso index.html)
+// isso abaixo é basicamente a criaćão de um componente com a id == #app
 createApp(App).mount('#app')
